615900a56ad07b0ecb8ec21519b47539a48a3d3e
seanconnery13 <dhavalscloud@gmail.com>
2025-02-24 18:20:22 -0500

    deref coercion

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ playground for RUST 🦀
   Creating a mutable reference is like grabbing hold of the lock (to the memory). 
   Post that no references (either mutable or immutable) can be created until the lifetime of the first mutable reference.
 * Calling join on the handle blocks the thread currently running until the thread represented by the handle terminates 
+* Deref coercion is an automatic reference to reference conversion. By implementing the deref trait on a type, you are defining behavior of how to dereference.
+  * Rust will then automatically dereference a type - when there is mismatch in a function
+  * test_deref_coercion(&mybox); fn test_deref_coercion(x : &String)
+  * dereference of mybox returns a reference to a string. So Rust automatically does that for you !
---
+++ src/bin/deref_coercion.rs
@@ -0,0 +1,39 @@
+use std::ops::Deref;
+
+fn main() {
+    // deref coercion
+    //so by implementing the deref trait
+    // we have added automatic conversion from &MyBox<T> to &T ? Yes
+    let mybox = MyBox::new(String::from("deref coercion works"));
+    test_deref_coercion(&mybox);
+}
+
+fn test_deref_coercion(x : &String) {
+    println!("{}", x);
+}
+
+// struct that stores T
+struct MyBox<T>(T);
+
+//adding behavior
+// add a new function that creates MyBox
+impl<T> MyBox<T> {
+    fn new(input: T) -> MyBox<T> {
+        MyBox(input)
+    }
+}
+
+// implement deref trait for MyBox
+impl<T> Deref for MyBox<T> {
+    // associated property
+    type Target =  T;
+    // takes a immutable reference to self
+    // and returns type T
+    // notice Self vs self
+    // Self refers to the type and self refers to an instance of that type
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+
