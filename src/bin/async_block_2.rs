ce10bbd4655fb3c251c52e62688c78c89433ae2e
seanconnery13 <dhavalscloud@gmail.com>
2025-03-09 14:35:15 -0400

    formatting - mar 9th 2025

--- src/bin/async_block_2.rs
+++ src/bin/async_block_2.rs
@@ -1,14 +1,14 @@
-use std::time::Duration;
 use futures::future::join;
+use std::time::Duration;
 use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
-    let f1 =  async {
-       for i in 1..10 {
-           println!("hi number {i} from the first task!");
-           sleep(Duration::from_millis(500)).await;
-       }
+    let f1 = async {
+        for i in 1..10 {
+            println!("hi number {i} from the first task!");
+            sleep(Duration::from_millis(500)).await;
+        }
     };
 
     let f2 = async {
@@ -16,7 +16,8 @@ async fn main() {
             println!("hi number {i} from the second task!");
             sleep(Duration::from_millis(500)).await;
         }
-    }.await;
+    }
+    .await;
 
     f1.await;
 }
